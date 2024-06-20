05ffefed1385176cc0613d55bca2d0561621dfff
TURBO70 <yassooff2003@gmail.com>
2024-06-20 23:10:47 +0300

    get service

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TasksService {
+private tasks=[];  //priv bc no one in the app can access it 
+
+getAllTasks(){
+    return this.tasks;
+}
+}
