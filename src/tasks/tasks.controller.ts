4268b8fa01792a26937dc2f713aa515d6d8d2579
TURBO70 <yassooff2003@gmail.com>
2024-06-21 01:34:01 +0300

    task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,18 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
-//we split controller from busness lgic bc its easy to test and maintain and 
+//we split controller from busness lgic bc its easy to test and maintain and
 //controller work is to recieve req and send response only
 
 @Controller('tasks')
 export class TasksController {
-constructor(private tasksService: TasksService) {}
-//this constructor achieve dp injection and singlton we inject service and if no
-//instance of TasksService we create one otherwise we dont which is the singlton
+  constructor(private tasksService: TasksService) {}
+  //this constructor achieve dp injection and singlton we inject service and if no
+  //instance of TasksService we create one otherwise we dont which is the singlton
 
-@Get()
-getAllTasks(){
+  @Get()
+  getAllTasks() {
     return this.tasksService.getAllTasks;
-}
-
+  }
 }
