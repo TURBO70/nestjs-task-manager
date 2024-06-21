310721942113a26e4061735ebefd7d326fd3be39
TURBO70 <yassooff2003@gmail.com>
2024-06-21 20:49:15 +0300

    create task service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 @Injectable()
 export class TasksService {
-  private tasks = []; //priv bc no one in the app can access it
+  private tasks: Task[] = []; //priv bc no one in the app can access it
 
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: '',
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
   }
 }
