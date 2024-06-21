c8a8191718bb06f4bb1e6d3ed5e549b5bcb45184
TURBO70 <yassooff2003@gmail.com>
2024-06-21 20:49:02 +0300

    create task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-
+import { Task } from './task.model';
+import { Body } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 //we split controller from busness lgic bc its easy to test and maintain and
 //controller work is to recieve req and send response only
 
@@ -11,7 +13,12 @@ export class TasksController {
   //instance of TasksService we create one otherwise we dont which is the singlton
 
   @Get()
-  getAllTasks() {
-    return this.tasksService.getAllTasks;
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+  //this is the get request to get all the tasks
+  @post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
   }
 }
