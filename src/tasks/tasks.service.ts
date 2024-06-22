11ba010fa7ef0b0cf54ed82c5426680a8cd1de3a
TURBO70 <yassooff2003@gmail.com>
2024-06-22 22:26:11 +0300

    another service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -9,6 +9,14 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  deleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
     const task: Task = {
