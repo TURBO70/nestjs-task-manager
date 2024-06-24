a0ecd6291a264cbe8ab0e3e8cc692b8c2b8d6990
TURBO70 <yassooff2003@gmail.com>
2024-06-25 00:24:40 +0300

    update task service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,31 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
+import { TaskStatus } from './task-status-enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = []; //priv bc no one in the app can access it
+  
 
-  getAllTasks(): Task[] {
-    return this.tasks;
-  }
+  // getAllTasks(): Task[] {
+  //   return this.tasks;
+  // }
 
-  getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
-  }
+  // getTaskById(id: string): Task {
+  //   return this.tasks.find((task) => task.id === id);
+  // }
 
-  deleteTaskById(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
-  }
+  // deleteTaskById(id: string): void {
+  //   this.tasks = this.tasks.filter((task) => task.id !== id);
+  // }
 
-  createTask(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto;
-    const task: Task = {
-      id: '',
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    };
-    this.tasks.push(task);
-    return task;
-  }
+  // createTask(createTaskDto: CreateTaskDto): Task {
+  //   const { title, description } = createTaskDto;
+  //   const task: Task = {
+  //     id: '',
+  //     title,
+  //     description,
+  //     status: TaskStatus.OPEN,
+  //   };
+  //   this.tasks.push(task);
+  //   return task;
+  // }
 }
