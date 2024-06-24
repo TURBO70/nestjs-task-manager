434d742fec921560dc8deb6852d988f388ec4614
TURBO70 <yassooff2003@gmail.com>
2024-06-25 00:23:53 +0300

    update task dto

---
+++ src/tasks/dto/update-task-status.dto.ts
@@ -0,0 +1,9 @@
+import { TaskStatus } from "../task-status-enum";
+import { IsEnum } from "class-validator";
+
+
+export class UpdateTaskStatusDto {
+  @IsEnum(TaskStatus)
+  status: TaskStatus;
+
+}
