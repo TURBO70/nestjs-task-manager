637098ba87493a989dfb871d5f100811b3e71069
TURBO70 <yassooff2003@gmail.com>
2024-06-25 00:24:28 +0300

    update task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
 import { Body } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 //we split controller from busness lgic bc its easy to test and maintain and
@@ -12,24 +11,24 @@ export class TasksController {
   //this constructor achieve dp injection and singlton we inject service and if no
   //instance of TasksService we create one otherwise we dont which is the singlton
 
-  @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
-  }
-  //this is the get request to get all the tasks
+  // @Get()
+  // getAllTasks(): Task[] {
+  //   return this.tasksService.getAllTasks();
+  // }
+  // //this is the get request to get all the tasks
 
-  @Get(':/id')
-  getTaskById(@Param('id') id: string): Task {
-    return this.tasksService.getTaskById(id);
-  }
+  // @Get(':/id')
+  // getTaskById(@Param('id') id: string): Task {
+  //   return this.tasksService.getTaskById(id);
+  // }
 
-  @Delete(':/id')
-  deleteTaskById(@Param('id') id: string): void {
-    return this.tasksService.deleteTaskById(id);
-  }
+  // @Delete(':/id')
+  // deleteTaskById(@Param('id') id: string): void {
+  //   return this.tasksService.deleteTaskById(id);
+  // }
 
-  @Post()
-  createTask(@Body() createTaskDto: CreateTaskDto): Task {
-    return this.tasksService.createTask(createTaskDto);
-  }
+  // @Post()
+  // createTask(@Body() createTaskDto: CreateTaskDto): Task {
+  //   return this.tasksService.createTask(createTaskDto);
+  // }
 }
