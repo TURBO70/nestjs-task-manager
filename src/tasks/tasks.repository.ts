b55075ca30e0a9556f4ab062891e7154e76cbc6d
TURBO70 <yassooff2003@gmail.com>
2024-06-25 22:38:14 +0300

    task repo

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,8 +1,19 @@
 import { EntityRepository, Repository } from "typeorm";
 import { Task } from "./task.entity";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { TaskStatus } from "./task-status-enum";
 
 @EntityRepository(Task)
-/**
- * Repository class for managing tasks.
- */
-export class TaskRepository extends Repository<Task> {}
+export class TaskRepository extends Repository<Task> {
+  async createTask(createTaskDto: CreateTaskDto) {
+    const { title, description } = createTaskDto;
+
+    const task = this.create({
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    });
+    await this.save(task);
+    return task;
+  }
+}
