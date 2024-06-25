3a8212c4201d9b388d9b11e7fbf715ead5a93768
TURBO70 <yassooff2003@gmail.com>
2024-06-25 22:08:29 +0300

    task service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,17 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { TaskStatus } from './task-status-enum';
-import { CreateTaskDto } from './dto/create-task.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { TaskStatus } from "./task-status-enum";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { TaskRepository } from "./tasks.repository";
+import { Task } from "./task.entity";
+
 @Injectable()
 export class TasksService {
-  
+  constructor(
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository,
+  ) {}
 
   // getAllTasks(): Task[] {
   //   return this.tasks;
   // }
 
-  // getTaskById(id: string): Task {
-  //   return this.tasks.find((task) => task.id === id);
-  // }
+  async getTaskById(id: string): Promise<Task> {
+    const found = await this.taskRepository.findOne(id);
+    if (!found) {
+      throw new NotFoundException(`Task with id ${id} is not found `);
+    }
+    return found;
+  }
 
   // deleteTaskById(id: string): void {
   //   this.tasks = this.tasks.filter((task) => task.id !== id);
